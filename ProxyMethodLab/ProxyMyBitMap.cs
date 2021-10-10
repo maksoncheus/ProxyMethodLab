8edfaef2343a8e4ff0486f3a1acca70b7919f9d9
maksoncheus <maksoncheus@gmail.com>
2021-10-11 01:03:30 +0700

    добавил проверку расширения

--- ProxyMethodLab/ProxyMyBitMap.cs
+++ ProxyMethodLab/ProxyMyBitMap.cs
@@ -19,25 +19,31 @@ namespace ProxyMethodLab
         {
             if (File.Exists(Path))
             {
-                Regex checkpath = new Regex("_\\d*x\\d*\\.bmp$");
-                if (checkpath.IsMatch(Path))
+                Regex checkext = new Regex("\\.bmp$");
+                if (checkext.IsMatch(Path))
                 {
-                    string _name = Path;
-                    while (_name.Contains("\\"))
+                    Regex checkpath = new Regex("_\\d*x\\d*\\.bmp$");
+
+                    if (checkpath.IsMatch(Path))
                     {
-                        _name = _name.Remove(0, _name.IndexOf('\\') + 1);
+                        string _name = Path;
+                        while (_name.Contains("\\"))
+                        {
+                            _name = _name.Remove(0, _name.IndexOf('\\') + 1);
+                        }
+                        _name = _name.Remove(_name.Length - 4, 4);
+                        _name = _name.Remove(0, _name.IndexOf('_') + 1);
+                        string[] dimensions;
+                        dimensions = _name.Split('x');
+                        return $"Size = {dimensions[0]}x{dimensions[1]}  {Path}";
                     }
-                    _name = _name.Remove(_name.Length - 4, 4);
-                    _name = _name.Remove(0, _name.IndexOf('_') + 1);
-                    string[] dimensions;
-                    dimensions = _name.Split('x');
-                    return $"Size = {dimensions[0]}x{dimensions[1]}  {Path}";
+                    else
+                    {
+                        MyBitMap img = new MyBitMap(Path);
+                        return img.GetDimensions();
+                    }
                 }
-                else
-                {
-                    MyBitMap img = new MyBitMap(Path);
-                    return img.GetDimensions();
-                }
+                else return "Not a bitmap";
             }
             else return "No such file";
             
